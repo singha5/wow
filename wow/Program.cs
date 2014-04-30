c9da10979b0f43c69267e3997d777fa4e216e4bd
singha5 <arunjitsingh6@gmail.com>
2014-04-30 09:57:07 -0400

    sync

---
+++ wow/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number:");
+            String input = Console.ReadLine();
+            int n = Convert.ToInt32(input);
+            
+            Console.Write("W");
+
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("o");
+                
+            }
+
+            Console.Write("W!");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
